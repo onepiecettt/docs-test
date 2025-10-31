e726a4e307997859670f59de0265c2191b367996
taylor <taylor@a.com>
2025-10-31 23:13:52 +0800

    IP

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.onepiece.net',
+  url: 'http://47.115.129.219',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
